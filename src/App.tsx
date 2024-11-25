e6f3081a2429390e2a6e44e1db790ac85043c0a2
Paul McKenna <paulmmckenna@gmail.com>
2024-11-25 21:42:32 +0000

    change hash router for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Calendar } from './components/Calendar';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.NODE_ENV === 'production' ? '' : '/'}>
       <div className="min-h-screen bg-gradient-to-b from-red-100 to-green-100">
         <Routes>
           <Route path="/" element={<Calendar />} />
