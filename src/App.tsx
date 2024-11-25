75423fda0abc6363bcd45874178d3261953c44e9
Paul McKenna <paulmmckenna@gmail.com>
2024-11-25 21:44:14 +0000

    Vercel changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Calendar } from './components/Calendar';
 
 function App() {
   return (
-    <Router basename={process.env.NODE_ENV === 'production' ? '' : '/'}>
+    <Router>
       <div className="min-h-screen bg-gradient-to-b from-red-100 to-green-100">
         <Routes>
           <Route path="/" element={<Calendar />} />
